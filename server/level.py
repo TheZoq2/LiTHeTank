66baf0addddbf2d1326688f44f05062d9f59bed7
Malcolm Wigren <trekommafem2@gmail.com>
2016-10-01 21:52:38 +0200

    Implemented all functions, I think

--- server/level.py
+++ server/level.py
@@ -1,5 +1,6 @@
 import sdl2.ext
 from vec import Vec2, vec2_from_direction
+import math
 import random
 
 FIRE_LEFT = 0
@@ -9,10 +10,11 @@ DEFAULT_ENEMY_SIZE = 5
 DEFAULT_BULLET_DAMAGE = 10
 DEFAULT_BULLET_SPEED = 2
 DEFAULT_ENEMY_HEALTH = 20
-SPAWN_FREQUENCY = 100
+SPAWN_FREQUENCY = 10
+TANK_SIZE = 16
 
 # The default probability for the enemies. Higher numbers result in lower frequencies
-DEFAULT_FIRING_FREQUENCY = 100
+DEFAULT_FIRING_FREQUENCY = 1
 
 
 class Enemy():
@@ -60,7 +62,7 @@ class Level():
         self._update_enemy_positions(delta_time)
         self._fire_enemies(delta_time)
         self._remove_dead_enemies()
-        # self._spawn_enemies(delta_time)
+        self._spawn_enemies(delta_time)
 
     def to_json(self):
         return json.dumps(self, default=lambda o: o.__dict__, sort_keys=True, indent=4)
@@ -72,7 +74,7 @@ class Level():
     
     def _handle_bullet_collisions(self):
         for bullet in self.bullets:
-            if bullet.position.is_within_bounds(self.tank.position, enemy.size):
+            if bullet.position.is_within_bounds(self.tank.position, TANK_SIZE):
                 self.tank.health -= bullet.damage
             for enemy in self.enemies:
                 if bullet.position.is_within_bounds(enemy.position, enemy.size):
@@ -83,7 +85,10 @@ class Level():
             bullet.position += bullet.velocity * delta_time
 
     def _update_enemy_positions(self, delta_time):
-        pass
+        for enemy in self.enemies:
+            enemy.angle = enemy.position.relative_angle_to(self.tank.position)
+            enemy.position += vec2_from_direction(enemy.angle, 
+                                                  1 * random.randint(-10, 10) * delta_time)
     
     def _remove_dead_enemies(self):
         self.enemies = [e for e in self.enemies if not e.is_dead()]
@@ -97,8 +102,7 @@ class Level():
                 self._enemy_fire(enemy)
                 
     def _enemy_fire(self, enemy):
-        angle = enemy.position.relative_angle_to(self.tank.position)
-        self.bullets.append(Bullet(enemy.position, angle))
+        self.bullets.append(Bullet(enemy.position, enemy.angle))
     
     def _spawn_enemies(self, delta_time):
         if not random.randint(0, int(SPAWN_FREQUENCY / delta_time)):
