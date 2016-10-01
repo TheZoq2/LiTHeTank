dd8400fd26e3cb71e94a53f3485cc13f2d7b0498
Frans Skarman <frans.skarman@gmail.com>
2016-10-01 12:09:52 +0200

    Add some simple socket tests

--- client/main.py
+++ client/main.py
@@ -1,5 +1,13 @@
+#!/bin/python
+import socket
 
-def main():
-    print("I is client")
+# create an INET, STREAMing socket
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# now connect to the web server on port 80 - the normal http port
+s.connect(("localhost", 2000))
 
-main()
+test = s.recv(1024)
+
+print(test);
+
+
--- server/main.py
+++ server/main.py
@@ -1,4 +1,24 @@
+#!/bin/python
+
+import socket;
+
+PORT = 2000
+
 def main():
-    print("I is server")
+    # create an INET, STREAMing socket
+    serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # bind the socket to a public host, and a well-known port
+    serversocket.bind(("", PORT))
+    # become a server socket
+    serversocket.listen(5)
+
+    print("Listening on port {}".format(PORT))
+
+    while True:
+        # accept connections from outside
+        (clientsocket, address) = serversocket.accept()
+        # now do something with the clientsocket
+        # in this case, we'll pretend this is a threaded server
+        clientsocket.send(b"HEEEEEEJ")
 
 main()
