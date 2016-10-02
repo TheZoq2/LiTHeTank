f1a6b5e5d122b405e41c36fb986f06cb9c3b71b7
Frans Skarman <frans.skarman@gmail.com>
2016-10-02 19:57:35 +0200

    Add centering of sprites

--- client/driver.py
+++ client/driver.py
@@ -47,15 +47,20 @@ def driver_main(renderer, factory, s):
     compass_needle = load_sprite("compass_needle.png", factory)
     compass_needle.x = 150
     compass_needle.y = 37
+    compass_needle.center = False
 
     compass_needle_small = load_sprite("compass_needle_small.png", factory)
     compass_needle_small.x = 225
     compass_needle_small.y = 135
+    compass_needle_small.center = False
 
     levers = {}
     levers["up"] = load_sprite("lever_up.png", factory)
     levers["down"] = load_sprite("lever_down.png", factory)
     levers["neutral"] = load_sprite("lever_neutral.png", factory)
+
+    for lever in levers:
+        levers[lever].center = True
 
     running = True
 
