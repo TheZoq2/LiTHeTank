8499df40bf752b6a570a37ee5acb516dcf76e9a4
Robin Sliwa <rorrosliwa@gmail.com>
2016-10-02 02:39:52 +0200

    added rotation keys for gunner

--- client/gunner.py
+++ client/gunner.py
@@ -1,4 +1,7 @@
 import json
+import math
+import sdl2
+import sdl2.ext
 from render_util import *
 from select import select
 from socket_util import *
@@ -9,11 +12,24 @@ def gunner_main(renderer, factory, s):
 
     background = load_sprite("gunner_background.png", factory)
     compass_needle = load_sprite("compass_needle.png", factory)
-    tank_angle = 0
+    compass_needle.x = 223
+    compass_needle.y = 41
+    gun_angle = 0
 
     running = True
 
     while running:
+
+        events = sdl2.ext.get_events()
+        for event in events:
+            if event.type == sdl2.SDL_QUIT:
+                running = False
+                break
+            if event.type == sdl2.SDL_KEYDOWN:
+                if event.key.keysym.sym == sdl2.SDLK_k:
+                    gun_angle += 1
+                elif event.key.keysym.sym == sdl2.SDLK_j:
+                    gun_angle -= 1
 
         events = sdl2.ext.get_events()
         for event in events:
@@ -33,7 +49,7 @@ def gunner_main(renderer, factory, s):
             for data in decoded_server_data:
                 loaded_data = json.loads(data)
                 if (loaded_data["type"] == "update"):
-                    tank_angle = loaded_data["data"]["tank"]["angle"]
+                    #tank_angle = loaded_data["data"]["tank"]["angle"]
                     break
 
             if not server_data:
@@ -43,5 +59,5 @@ def gunner_main(renderer, factory, s):
             if server_data == b"exit":
                 running = False
 
-        compass_needle.angle = -tank_angle
-        renderer.render([background, compass_needle])
+        compass_needle.angle = -gun_angle
+        render_sprites([background, compass_needle], renderer)
